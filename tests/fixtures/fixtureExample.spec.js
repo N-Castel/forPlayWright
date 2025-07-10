73d415ed4cecb404864642d400635616471150b1
N-Castel <NicoCastel123@gmail.com>
2025-07-10 11:09:05 -0300

    add ToDo test
    add toDo test

--- tests/fixtures/fixtureExample.spec.js
+++ tests/fixtures/fixtureExample.spec.js
@@ -1,19 +1,22 @@
 const {test, expect} = require("../fixtures/pages")
 const dataFix = require("../fixtures/fixtureToDo.json")
 
-test("Add To Do", async({toDoFixture, page}) => {
-   
-    // await todoPageFixture.addTodo(dataFix.data1)
-    // await todoPageFixture.addTodo(dataFix.data2)
+test.describe('toDo test', () => {
 
-    await expect(toDoFixture.toDoList).toContainText([
-        dataFix.data1,
-        dataFix.data2
-    ])
+    test("Add To Do", async({toDoFixture, page}) => { // resolve what's in toDoFixture then continue with await statements
 
-    await expect(toDoFixture.Itemslabel).toContainText(dataFix.itemsList)
+        await expect(toDoFixture.toDoList).toContainText([
+            dataFix.data1,
+            dataFix.data2
+        ])
+    })
+
+    test('Validate todo labels is updated', async ({toDoFixture, page}) => {
+
+        await toDoFixture.addTodo(dataFix.data3)
+        await toDoFixture.addTodo(dataFix.data4)
+
+        await expect(toDoFixture.Itemslabel).toContainText(dataFix.itemsList)
+
+    })
 })
-
-// test('Validate todo labels is updated', async ({todoPageFixture}) => {
-//     await todoPageFixture.addTodo()
-// })
--- tests/fixtures/pages.js
+++ tests/fixtures/pages.js
@@ -1,23 +1,23 @@
 /**
- * Esta clase sería como la de commands en cypress, en la cual creamos los test que pueden ser reutilizados desde las clase test del frame
+ * this class would be like cy commands, to create methods that can be used in the whole e2e(fixture) 
  */
 
-const base = require("@playwright/test");
-const { ToDoPageFile } = require("../fixtures/todo-page");
+const base = require("@playwright/test")
+const { ToDoPageFile } = require("../fixtures/todo-page")
 const dataFix = require("../fixtures/fixtureToDo.json")
 
 
-exports.test = base.test.extend({ //en cypress seria Cypress.Commands.add() ya que esta creando la versiòn personalizada del test y la extiende a el fixture que vamos a crear en el siguiete statement
-    toDoFixture: async({page}, use ) => { // creamos el fixture y le dicemos que va a necesitar el page y el use
+exports.test = base.test.extend({ //this statement its like  Cypress.Commands.add() to create a version of my test  and extend the fixture that we are creating in the next statement 
+    toDoFixture: async({page}, use ) => { // ccreate the fixture (toDoFixture) and tell him that it will be need the "page"
 
-        const toDo = new ToDoPageFile(page); //instancia el page object 
+        const toDo = new ToDoPageFile(page); //intance the object in toDo
         
         await toDo.goto();
-        await toDo.addTodo(dataFix.data1);
-        await toDo.addTodo(dataFix.data2);
+        await toDo.addTodo(dataFix.data1)
+        await toDo.addTodo(dataFix.data2)
 
-        await use(toDo); //inyecta toDo en toDoFixture
-    },
-});
+        await use(toDo); //injects toDo in toDofixture
+    }
+})
 
 exports.expect = base.expect
