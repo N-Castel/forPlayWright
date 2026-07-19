bc4082b3416d9dad1ea1b0bce6d228446f1ae590
N-Castel <119272509+N-Castel@users.noreply.github.com>
2026-07-18 23:56:59 -0500

    AutomationExcercise - Done
    Test completed and working as expected

--- e2e/tests/automationExercise/page.js
+++ e2e/tests/automationExercise/page.js
@@ -1,5 +1,5 @@
-class AutomationPage{
 
+class AutomationPage{
     /**
      * @param {import('playwright/test').Page} playwrightPage
      */
@@ -7,45 +7,53 @@ class AutomationPage{
     constructor(playwrightPage){
         this.page = playwrightPage;
 
-        this.productLink = this.page.locator('a[href="/products"]');
-        this.fillSearchProduct = this.page.getByTestId('search_product');
-        this.clickSubmitSearch = this.page.getByTestId('submit_search');
-        this.clickAddCartItem = (productName) => this.page.locator('.productInfo', {hasText: productName});
-        this.clickContinueShoppingButton = this.page.getByRole('button', {name: "Continue Shopping"});
-        this.productAddedLabel = this.page.getByText('Your product has been added to cart.');
-        this.clickCartButton = this.page.getByRole('link', {name: 'Cart'});
-        this.itemAddedinCart = this.page.locator('h4 > a[href="/product_details/28"]');
+        this.productsButton = this.page.locator('li > a[href="/products"]');
+        this.searchInput = this.page.getByTestId('search_product');
+        this.submitSearchButton = this.page.getByTestId('submit_search');
+
+        this.productByName = (productName) =>
+            this.page.locator('.single-products').filter({
+                has: this.page.locator('p', { hasText: new RegExp(`^${productName}$`)})
+        });
+
+        this.addProductCartButton = this.page.locator('a.add-to-cart');
+        this.cartButton = this.page.locator('.modal-body a')
+
+        this.itemCartProductByName = (productName) => this.page.locator('h4 > a', {hasText: productName });
     }
 
     async navigate(){
-        await this.page.goto('');
-    }
+        await this.page.goto('')
+    } 
 
-    async clickProductLink(){
-        await this.productLink.click();
-    }
-
-    /**
-     * @param {string} value
-     */
-    async searchProduct(value){
-        await this.fillSearchProduct.fill(value);
-        await this.clickSubmitSearch.click();
-    }
-
-    async addProductCart(productName){
-        const card = this.clickAddCartItem(productName)
-        await card.locator('a.add-to-cart').click();
-    }
-
-    async clickContinueShopping(){ 
-        await this.clickContinueShoppingButton.click();
-    }
-
-    async viewCart(){
-        await this.clickCartButton.click();
+    async clickProductsButton(){
+        await this.productsButton.click();
         
+    }
+    /**
+     * @param {string} productName
+     */
+
+    async searchProduct(productName){
+        await this.searchInput.fill(productName);
+        await this.submitSearchButton.click();
+    }
+
+    async addItemCart(productName){
+        const item = this.productByName(productName);
+        await item.locator('.product-overlay a.add-to-cart').click();
+    }
+
+    async clickCartButton(){
+        await this.cartButton.click();
+    }
+    /**
+     * 
+     * @param {string} productName 
+     */
+    async cartItemAdded(productName){
+        return this.itemCartProductByName(productName);
     }
 }
 
-module.exports = { AutomationPage }
+module.exports = {AutomationPage}
--- e2e/tests/automationExercise/test.spec.js
+++ e2e/tests/automationExercise/test.spec.js
@@ -1,11 +1,3 @@
-/**
- * navigate to page
- * click product link
- * search product
- * add product cart
- * validate item added
- */
-
 const {test, expect} = require("@playwright/test");
 const { AutomationPage } = require('./page');
 const data = require('./data.json')
@@ -15,14 +7,14 @@ test('Add ítem to cart and validate item added', async ({page}) => {
     const testPage = new AutomationPage(page);
 
     await testPage.navigate();
-    await testPage.clickProductLink();
+
+    await testPage.clickProductsButton();
     await expect(page).toHaveURL(new RegExp(data.urlProduct));
 
     await testPage.searchProduct(data.nameProduct);
-    await testPage.addProductCart(data.nameProduct);
-    await expect(testPage.productAddedLabel).toContainText(data.productAddedCart);
+    await testPage.addItemCart(data.nameProduct);
+    await testPage.clickCartButton();
+    await expect(page).toHaveURL(new RegExp(data.urlCart));
 
-    await testPage.clickContinueShopping();
-    await testPage.viewCart();
-    await expect(testPage.itemAddedinCart).toHaveText(data.cottonVShirt);
+    await expect(testPage.itemCartProductByName(data.nameProduct)).toContainText(new RegExp(data.nameProduct));
 })
